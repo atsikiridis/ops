6dd3b9a9c7715138f78c22dc41e32e3a06e048c7
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-07-29 17:27:08 +0200

    BAI: No name change in most relevant name. Removed create_normalized_name.

--- modules/bibauthorid/lib/bibauthorid_name_utils.py
+++ modules/bibauthorid/lib/bibauthorid_name_utils.py
@@ -966,18 +966,18 @@ def most_relevant_name(name_variants):
         return None
     name_parts_list = list()
 
-    for name in name_variants:
-        name_parts_list.append(split_name_parts(name))
+    for index, name in enumerate(name_variants):
+        name_parts_list.append((index, split_name_parts(name)))
 
     sorted_by_initials = sorted(name_parts_list,
-                                key=lambda k: len(k[1]),
+                                key=lambda k: len(k[1][1]),
                                 reverse=True)
                                  
-    sorted_by_relevance_name_list = sorted(sorted_by_initials,
-                                           key=lambda k: len(k[2]),
-                                           reverse=True)
+    most_relevant_name_idx = sorted(sorted_by_initials,
+                                    key=lambda k: len(k[1][2]),
+                                    reverse=True)[0][0]
 
-    return create_normalized_name(sorted_by_relevance_name_list[0])
+    return name_variants[most_relevant_name_idx]
 
 GLOBAL_gendernames = _load_gender_firstnames_dict()
 GLOBAL_name_variations = _load_firstname_variations()
